302c77f2b490623a2a655a25c0ae08ebfe973c02
Sameer Shaikh <sameershaikh01072004@gmail.com>
2023-08-15 02:42:16 +0530

    Home Page Frontent Completed

--- app/src/main/java/com/agpitcodeclub/app/Dashboard.java
+++ app/src/main/java/com/agpitcodeclub/app/Dashboard.java
@@ -1,14 +1,84 @@
 package com.agpitcodeclub.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.agpitcodeclub.app.fragments.Announcement;
+import com.agpitcodeclub.app.fragments.Community;
+import com.agpitcodeclub.app.fragments.Home;
+import com.agpitcodeclub.app.fragments.Post;
+import com.agpitcodeclub.app.fragments.Profile;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Dashboard extends AppCompatActivity {
-
+    private BottomNavigationView nav;
+    private FrameLayout mainframe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        initui();
+
+        navbar();
+    }
+
+    public void loadFragment(Fragment fragment, boolean flag){
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft= fm.beginTransaction();
+        if(flag) {
+            ft.add(R.id.dashboardframe, fragment);
+        }
+        else {
+            ft.replace(R.id.dashboardframe,fragment);
+        }
+        ft.commit();
+
+    }
+
+    private void initui() {
+        nav=findViewById(R.id.btm_nav_main);
+        mainframe=findViewById(R.id.dashboardframe);
+        nav.setSelectedItemId(R.id.home);
+
+    }
+
+    private void navbar() {
+        nav.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                switch (id) {
+                    case R.id.home:
+                        loadFragment(new Home(), true);
+                        break;
+                    case R.id.community:
+                        loadFragment(new Community(), false);
+                        break;
+                    case R.id.post:
+                        loadFragment(new Post(), false);
+                        break;
+                    case R.id.announcement:
+                        loadFragment(new Announcement(), false);
+                        break;
+                    case R.id.profile:
+                        loadFragment(new Profile(), false);
+                        break;
+                    default:
+                        Toast.makeText(Dashboard.this, "btm", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
     }
 }
---
+++ app/src/main/java/com/agpitcodeclub/app/fragments/Home.java
@@ -0,0 +1,70 @@
+package com.agpitcodeclub.app.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.agpitcodeclub.app.R;
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Home#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Home extends Fragment {
+    private ImageSlider imageSlider;
+
+    public Home() {
+        // Required empty public constructor
+    }
+
+    public static Home newInstance(String param1, String param2) {
+        Home fragment = new Home();
+        return fragment;
+    }
+    @Override
+    public void onViewCreated( View view, @Nullable Bundle savedInstanceState) {
+        initUi(view);
+        sliderImage();
+    }
+
+    private void initUi(View view) {
+        imageSlider=view.findViewById(R.id.intro_img_slider);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    private void sliderImage() {
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel("https://firebasestorage.googleapis.com/v0/b/page1-f073e.appspot.com/o/demo%2FBest_Principal.jpg?alt=media&token=dd7ea6a2-ea1a-4028-b3f2-93be886c3d43", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://firebasestorage.googleapis.com/v0/b/page1-f073e.appspot.com/o/demo%2Fprin.jpg?alt=media&token=2bd88912-c544-4829-a179-6a03b5ed4ac2", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://firebasestorage.googleapis.com/v0/b/page1-f073e.appspot.com/o/demo%2Finfra.jpg?alt=media&token=e4a1cbe6-a15e-44f1-a5fa-5a1d225604b9", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://firebasestorage.googleapis.com/v0/b/page1-f073e.appspot.com/o/demo%2Flab.jpg?alt=media&token=7dce08e1-aa67-4295-8a9b-fb7ab26f5ae3", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://firebasestorage.googleapis.com/v0/b/page1-f073e.appspot.com/o/demo%2Fmec.jpg?alt=media&token=0cc17984-17d4-46b3-a347-a80e752186f7", ScaleTypes.FIT));
+        imageSlider.setImageList(slideModels,ScaleTypes.FIT);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
