1cc983de1bad0657971154ed2e5e451c21676299
Sameer Shaikh <sameershaikh01072004@gmail.com>
2023-08-20 14:16:17 +0530

    Post fragment item and uploading post frontend completed

---
+++ app/src/main/java/com/agpitcodeclub/app/credentials/UploadPost.java
@@ -0,0 +1,16 @@
+package com.agpitcodeclub.app.credentials;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.agpitcodeclub.app.R;
+
+public class UploadPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_post);
+    }
+}
